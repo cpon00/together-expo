df237b9424bb7313a6a4d2133405bb60164f15ce
tigerlillyz <tigerlillyzietz@gmail.com>
2020-11-29 15:52:19 -1000

    Added Not Yet Implemented alert

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { Card } from 'react-native-elements'
 import styles from './styles'
 import Comments from "./Comments";
@@ -10,9 +10,12 @@ const Feed = props => {
       <Text style={styles.title}>{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}together</Text>
       <Card borderRadius={15}>
         <Text style={styles.username}>@stemmy</Text>
-        <View style={styles.posts}>
-          <Image source={require('../assets/coders.jpg')} style={styles.posts} />
+        <View style={styles.posts} >
+          <TouchableOpacity onPress={() => alert('Not Yet Implemented')}>
+            <Image source={require('../assets/coders.jpg')} style={styles.posts} />
+          </TouchableOpacity>
         </View>
+
         <Text style={styles.captions}>Just started a new job at Google as a Data Scientist! Here's how I did it... #programmer</Text >
         <Comments />
       </Card>
@@ -23,6 +26,11 @@ const Feed = props => {
       </Card>
     </View>
   )
+}
+
+
+const handlePostClick = () => {
+  console.log('Not Yet Implemented')
 }
 
 
